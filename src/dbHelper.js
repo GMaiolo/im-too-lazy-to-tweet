9242807425b0f73d2d76f5e6f87a7be67f4167e8
GMaiolo <guille.maiolo@gmail.com>
2018-01-13 14:50:57 -0300

    fix for mongodb uri in production

--- src/dbHelper.js
+++ src/dbHelper.js
@@ -1,5 +1,4 @@
 const mongo = require('mongodb').MongoClient
-const { mongoDbUri } = process.env.MONGODB_URI || require('../data/secret')
 
 async function getRandomQuote(quotesCollection) {
   return new Promise((resolve, reject) => {
@@ -24,6 +23,9 @@ async function updateTweetedStatus(quotesCollection, { _id }) {
 }
 
 async function getQuotesCollection() {
+  const mongoDbUri = process.env.NODE_ENV === 'PROD' 
+    ? process.env.MONGODB_URI
+    : require('../data/secret').mongoDbUri
   const database = await mongo.connect(mongoDbUri)
   return database
     .db('im-to-lazy-to-tweet-db')
