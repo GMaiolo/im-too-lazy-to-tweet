73529c3aa0510c04c3491b6ce20a4c00009a32e6
GMaiolo <guille.maiolo@gmail.com>
2018-04-09 22:53:34 +0000

    added cron capabilities

--- runner.js
+++ runner.js
@@ -1,5 +1,10 @@
 const lazy = require('./src/lazy')
+const cron = require('node-cron')
 
 console.log(`\nRunning on ${process.env.NODE_ENV || 'DEV'} environment\n`)
 
-lazy.tweetSomething()
+cron.schedule('0 14 * * *', function(){
+  console.log(`Running on: ${new Date()}`)
+  lazy.tweetSomething()
+})
+
